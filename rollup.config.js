cbbbc6044906938b4ba4e9f9af579bfcee401ad8
Simon Holmes <srsholmes@gmail.com>
2023-03-13 21:09:05 +0000

    chore: remove source map

--- rollup.config.js
+++ rollup.config.js
@@ -14,12 +14,12 @@ export default [
       {
         file: packageJson.main,
         format: 'cjs',
-        sourcemap: true,
+        sourcemap: false,
       },
       {
         file: packageJson.module,
         format: 'esm',
-        sourcemap: true,
+        sourcemap: false,
       },
     ],
     plugins: [
